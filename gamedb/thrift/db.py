4d939c768d74eff1d0949961054f8cc45f8856c1
Jason Martin <jolierouge242@gmail.com>
2025-11-19 13:17:21 +0000

    Add create database if not exists

--- gamedb/thrift/db.py
+++ gamedb/thrift/db.py
@@ -44,8 +44,10 @@ class DB:
     def create_attribute_table(
         self,
         database: str,
-    ) -> str:
-        return f"""CREATE TABLE IF NOT EXISTS {database}.attributes (
+    ) -> list[str]:
+        return [
+            f"CREATE DATABASE IF NOT EXISTS {database};",
+            f"""CREATE TABLE IF NOT EXISTS {database}.attributes (
                 id BIGINT PRIMARY KEY,
                 internal_name VARCHAR(255) NOT NULL,
                 visible BOOLEAN NOT NULL,
@@ -57,88 +59,110 @@ class DB:
                 vector3_z DOUBLE,
                 asset_id BIGINT,
 
-            );"""
+            );""",
+        ]
 
     def create_owner_table(
         self,
         database: str,
-    ) -> str:
-        return f"""CREATE TABLE IF NOT EXISTS {database}.attribute_owners (
+    ) -> list[str]:
+        return [
+            f"CREATE DATABASE IF NOT EXISTS {database};",
+            f"""CREATE TABLE IF NOT EXISTS {database}.attribute_owners (
                 id BIGINT AUTO_INCREMENT PRIMARY KEY,
                 attribute_id BIGINT NOT NULL,
                 mobile_id BIGINT,
                 item_id BIGINT,
                 asset_id BIGINT,
                 FOREIGN KEY (attribute_id) REFERENCES {database}.attributes(id)
-            );"""
+            );""",
+        ]
 
     def create_item_table(
         self,
         database: str,
-    ) -> str:
-        return f"""CREATE TABLE IF NOT EXISTS {database}.items (
+    ) -> list[str]:
+        return [
+            f"CREATE DATABASE IF NOT EXISTS {database};",
+            f"""CREATE TABLE IF NOT EXISTS {database}.items (
                 id BIGINT PRIMARY KEY,
                 internal_name VARCHAR(255) NOT NULL,
                 max_stack_size BIGINT,
                 item_type VARCHAR(50) NOT NULL,
                 blueprint_id BIGINT
-            );"""
+            );""",
+        ]
 
     def create_item_blueprint_table(
         self,
         database: str,
-    ) -> str:
-        return f"""CREATE TABLE IF NOT EXISTS {database}.item_blueprints (
+    ) -> list[str]:
+        return [
+            f"CREATE DATABASE IF NOT EXISTS {database};",
+            f"""CREATE TABLE IF NOT EXISTS {database}.item_blueprints (
                 id BIGINT PRIMARY KEY,
                 bake_time_ms BIGINT NOT NULL
-            );"""
+            );""",
+        ]
 
     def create_item_blueprint_component_table(
         self,
         database: str,
-    ) -> str:
-        return f"""CREATE TABLE IF NOT EXISTS {database}.item_blueprint_components (
+    ) -> list[str]:
+        return [
+            f"CREATE DATABASE IF NOT EXISTS {database};",
+            f"""CREATE TABLE IF NOT EXISTS {database}.item_blueprint_components (
                 id BIGINT AUTO_INCREMENT PRIMARY KEY,
                 item_blueprint_id BIGINT NOT NULL,
                 component_item_id BIGINT NOT NULL,
                 ratio DOUBLE NOT NULL,
                 FOREIGN KEY (item_blueprint_id) REFERENCES {database}.item_blueprints(id)
-            );"""
+            );""",
+        ]
 
     def create_inventory_table(
         self,
         database: str,
-    ) -> str:
-        return f"""CREATE TABLE IF NOT EXISTS {database}.inventories (
+    ) -> list[str]:
+        return [
+            f"CREATE DATABASE IF NOT EXISTS {database};",
+            f"""CREATE TABLE IF NOT EXISTS {database}.inventories (
                 id BIGINT PRIMARY KEY,
                 owner_id BIGINT NOT NULL,
                 owner_type VARCHAR(50),
                 max_entries BIGINT NOT NULL,
                 max_volume DOUBLE NOT NULL,
                 last_calculated_volume DOUBLE DEFAULT 0.0
-            );"""
+            );""",
+        ]
 
     def create_inventory_entry_table(
         self,
         database: str,
-    ) -> str:
-        return f"""CREATE TABLE IF NOT EXISTS {database}.inventory_entries (
+    ) -> list[str]:
+        return [
+            f"CREATE DATABASE IF NOT EXISTS {database};",
+            f"""CREATE TABLE IF NOT EXISTS {database}.inventory_entries (
                 id BIGINT AUTO_INCREMENT PRIMARY KEY,
                 inventory_id BIGINT NOT NULL,
                 item_id BIGINT NOT NULL,
                 quantity DOUBLE NOT NULL,
                 is_max_stacked BOOLEAN DEFAULT FALSE,
                 FOREIGN KEY (inventory_id) REFERENCES {database}.inventories(id)
-            );"""
+            );""",
+        ]
 
     def create_mobile_table(
         self,
         database: str,
-    ) -> str:
-        return f"""CREATE TABLE IF NOT EXISTS {database}.mobiles (
+    ) -> list[str]:
+        return [
+            f"CREATE DATABASE IF NOT EXISTS {database};",
+            f"""CREATE TABLE IF NOT EXISTS {database}.mobiles (
                 id BIGINT PRIMARY KEY,
                 mobile_type VARCHAR(50) NOT NULL
-            );"""
+            );""",
+        ]
 
     
     def save_attribute(
