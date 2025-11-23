2745d8e9ab18527beaebfe86df66c22f1442a01a
Jason Martin <jolierouge242@gmail.com>
2025-11-23 15:16:16 +0000

    remove unused files and structure from earlier in the process

--- libs/i18n/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -I ..\ -wd4201 -wd4100 -wd4189 -DBUILD_INTERNAL=1 -DBUILD_SLOW=1 -DBUILD_WIN32=1 -FC -Z7 
-set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib xinput.lib ..\..\..\SDL3\lib\x86\SDL3.lib
-
-pushd build
-cl %CommonCompilerFlags% ..\examples\sdl.cpp /std:c++20 /EHsc /link %CommonLinkerFlags%
-popd
--- libs/i18n/i18n.hpp
+++
@@ -1,9 +0,0 @@
-#include "common/types.hpp"
-
-// This is an internal callback function pointer, the first
-// time through, we establish which language we are using, 
-// and set the callback, so that we don't need to test the string
-// every time
-typedef int (*__internal_i18n_callback)(string);
-
-int _t_en(string key, mut_string dest, u32 len);
